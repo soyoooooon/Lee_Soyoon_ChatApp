bfb5569bc2f5bce70a65c33eedcf5708ddc622fd
이소윤 <s_lee77176@fanshaweonline.ca>
2020-03-06 13:15:38 -0500

    initial build

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const io =require('socket.io')();
+
+const app =  express();
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+const server = app.listen(port, () => {
+    console.log(`app is running on port ${port}`);
+});
