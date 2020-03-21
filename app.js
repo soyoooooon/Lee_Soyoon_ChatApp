a1b5ec23394c247be6406fa802178c36cac964c5
이소윤 <s_lee77176@fanshaweonline.ca>
2020-03-20 21:32:00 -0400

    done

--- app.js
+++ app.js
@@ -12,5 +12,57 @@ app.get('/', (req, res) => {
 });
 
 const server = app.listen(port, () => {
-    console.log(`app is running on port ${port}`);
+   
 });
+
+
+io.attach(server);
+
+io.on('connection',(socket) => {
+  console.log('user connected');
+ 
+ 
+ 
+  socket.on('Created',(data)=>{
+    socket.broadcast.emit('Created',(data))
+
+  })
+
+  socket.on('urName',(data)=>{
+    socket.broadcast.emit('urName',(data))
+  })
+
+  socket.on('chatMessage', (data) =>{
+
+    socket.broadcast.emit('chatMessage',(data))
+  })
+
+
+  socket.on('typing', (data) =>{
+
+    socket.broadcast.emit('typing',(data))
+  })
+
+  socket.on('stopTyping', (data) =>{
+
+    socket.broadcast.emit('stopTyping',(data))
+  })
+
+ 
+
+  })
+
+
+  
+
+  
+  
+
+  
+   
+
+
+
+  
+
+
---
+++ public/js/main_vm.js
@@ -0,0 +1,68 @@
+const socket = io();
+
+let app = new Vue
+({
+
+    
+    el:'#app',
+
+    data:{
+        newMessage:null,
+        messages: [],
+        typing: false,
+        urName: null,
+        ready: false,
+     
+    },
+    watch:{
+        newMessage(value){
+
+            value ? socket.emit('typing'):socket.emit('stopTyping')
+
+        }
+
+
+    },
+
+    methods:{
+
+        dispatchMessage(){
+            this.messages.push({message: this.newMessage, type : 0 })
+            socket.emit('chatMessage', this.newMessage)
+             this.newMessage = null
+            // name: this.urName || "anonymous"
+
+            
+        },
+
+        addName(){
+            this.ready = true
+            socket.emit('joined',this.urName)
+        }
+    },
+    created(){
+
+        socket.emit('Created', 'Soyoon')
+        socket.on('Created', (data) =>{
+            console.log(data);
+        })
+
+        socket.on('chatMessage', (data)=>{
+            this.messages.push({message: data, type : 1 })
+
+        })
+
+        socket.on('typing',()=>{
+            this.typing = true;
+        })
+
+        socket.on('stopTyping',()=>{
+            this.stopTyping = false;
+        })
+
+        
+    }
+
+    
+
+})
